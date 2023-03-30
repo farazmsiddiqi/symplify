6e93aeeab190e97d97a0366572d73efb851d2d19
MinhPhan8803 <minhnp2@illinois.edu>
2023-03-29 22:42:25 -0500

    Backend stuff

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,17 +2,21 @@ from flask import Flask
 from flask_mysqldb import MySQL
 import os
 from dotenv import load_dotenv
+import requests
 
 app = Flask(__name__)
 
 load_dotenv()
 SQL_INSTANCE_PASS = os.getenv('PASSWORD')
+SQL_INSTANCE_USER = os.getenv('MYSQL_USER')
+SQL_INSTANCE_HOST = os.getenv('MYSQL_HOST')
+SQL_INSTANCE_DB = os.getenv('MYSQL_DB')
 
 # Configure MySQL connection
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'mydatabase'
+app.config['MYSQL_HOST'] = SQL_INSTANCE_HOST
+app.config['MYSQL_USER'] = SQL_INSTANCE_USER
+app.config['MYSQL_PASSWORD'] = SQL_INSTANCE_PASS
+app.config['MYSQL_DB'] = SQL_INSTANCE_DB
 
 # Create MySQL object
 mysql = MySQL(app)
@@ -20,31 +24,21 @@ mysql = MySQL(app)
 # Flask stuff
 @app.route('/')
 def hello_world():
-    return 'fasd, World!'
-
-@app.route('/insert', methods=["POST"])
-def insert_records():
-     # Get data from request body
-    data = request.get_json()
-
-    # Extract data fields
-    name = data['name']
-    age = data['age']
-
     # Create MySQL cursor
     cur = mysql.connection.cursor()
 
     # Execute query to insert data into database
-    query = "INSERT INTO mytable (name, age) VALUES (%s, %s)"
-    values = (name, age)
-    cur.execute(query, values)
+    # cur.execute('USE symplify-database')
+    cur.execute('SELECT * FROM Symptom')
+    rv = cur.fetchall()
+    return str(rv)
 
-    # Commit changes to database and close connection
-    mysql.connection.commit()
-    cur.close()
-
-    # Return success message
-    return 'not implemented'
+@app.route('/insert', methods=["POST"])
+def insert_records():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Symptom')
+    rv = cur.fetchall()
+    return str(rv)
 
 
 
