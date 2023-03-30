496af38addeae2ef76274ad2a01016d9c73631a6
farazmsiddiqi <farazs27@gmail.com>
2023-03-30 00:33:06 -0500

    update backend with insert ops

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_mysqldb import MySQL
 import os
 from dotenv import load_dotenv
@@ -35,13 +35,109 @@ def hello_world():
 
 @app.route('/insert', methods=["POST"])
 def insert_records():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM Symptom')
-    rv = cur.fetchall()
-    return str(rv)
+  data = request.get_json()
+  table_name = data['table']
+
+  weak_trackable_table_names = ['Condition', 'Symptom', 'Treatment', 'Tag', 'Weather']
+
+  if table_name == "User": 
+    return handle_user_table(data['user_id'], data['username'], data['password'], data['email'])
+  elif table_name in weak_trackable_table_names:
+    return handle_weak_trackable_tables(table_name, data['trackable_id'], data['trackable_name'], data['trackable_value'])
+  elif table_name == "Trackable":
+    return handle_trackable_table(data['trackable_id'], data['trackable_type'])
+  elif table_name == "Demographics":
+    return handle_demographics_table(data['user_id'], data['age'], data['sex'], data['country'])
+  elif table_name == "UserTracks":
+    return handle_usertracks_table(data['trackable_id'], data['user_id'])
+
+  cur = mysql.connection.cursor()
+  cur.execute('SELECT * FROM Symptom')
+  rv = cur.fetchall()
+  return str(rv)
 
 
+def handle_trackable_table(trackable_id, trackable_type):
+  cur = mysql.connection.cursor()
+  query = f"INSERT INTO Trackable (trackable_id, trackable_type) VALUES (CAST('{trackable_id}' AS SIGNED), '{trackable_type}')"
 
+  try:
+    cur.execute(query)
+  
+  except Exception as e:
+    return str(e), 500
+  
+  mysql.connection.commit()
+  cur.close()
+
+  return "Success", 200
+
+
+def handle_demographics_table(user_id, age, sex, country):
+  cur = mysql.connection.cursor()
+  query = f"INSERT INTO Demographics (user_id, age, sex, country) VALUES ('{user_id}', CAST('{age}' AS SIGNED), '{sex}', '{country}')"
+
+  try:
+    cur.execute(query)
+  
+  except Exception as e:
+    return str(e), 500
+  
+  mysql.connection.commit()
+  cur.close()
+
+  return "Success", 200
+
+
+def handle_usertracks_table(trackable_id, user_id):
+  cur = mysql.connection.cursor()
+  query = f"INSERT INTO UserTracks (trackable_id, user_id) VALUES (CAST('{trackable_id}' AS SIGNED), '{user_id}')"
+
+  try:
+    cur.execute(query)
+  
+  except Exception as e:
+    return str(e), 500
+  
+  mysql.connection.commit()
+  cur.close()
+
+  return "Success", 200
+
+
+def handle_weak_trackable_tables(table_name, trackable_id, trackable_name, trackable_value):
+  cur = mysql.connection.cursor()
+  query = f"INSERT INTO {table_name} (trackable_id, trackable_name, trackable_value) VALUES (CAST('{trackable_id}' AS SIGNED), '{trackable_name}', '{trackable_value}')"
+
+  if table_name == "Tag":
+    # Tag doesn't need trackable_value
+    query = f"INSERT INTO {table_name} (trackable_id, trackable_name) VALUES (CAST('{trackable_id}' AS SIGNED), '{trackable_name}')"
+  
+  try:
+    cur.execute(query)
+  
+  except Exception as e:
+    return str(e), 500
+  
+  mysql.connection.commit()
+  cur.close()
+
+  return "Success", 200
+
+def handle_user_table(user_id, username, password, email):
+  cur = mysql.connection.cursor()
+  query = f"INSERT INTO User (user_id, username, password, email) VALUES ('{user_id}', '{username}', '{password}', '{email}')"
+  
+  try:
+    cur.execute(query)
+  
+  except Exception as e:
+    return str(e), 500
+  
+  mysql.connection.commit()
+  cur.close()
+
+  return "Success", 200
 
 if __name__ == '__main__':
     app.run()
